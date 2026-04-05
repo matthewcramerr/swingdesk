b8d87bfbebad2e4c7dd3fdf9cd68c3abadf9750f
Matthew Cramer <matthewcramerr@gmail.com>
2026-04-05 13:46:16 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#05080c" />
+        <title>SwingDesk</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
       <Footer />
